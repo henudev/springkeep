5f0710ecaa1ae5a34a235a23d3577ef3f025d423
qinzhengzz <henuqin@gmail.com>
2019-05-18 15:56:46 +0800

    增加  mybatis-plus

--- ling/src/main/java/com/zfl/ling/TestOrder.java
+++ ling/src/main/java/com/zfl/ling/TestOrder.java
@@ -1,8 +1,13 @@
 package com.zfl.ling;
 
+import com.zfl.ling.dao.UserMapper;
+import com.zfl.ling.entity.User;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * @ClassName TestOrder
@@ -14,9 +19,13 @@ import org.springframework.stereotype.Component;
 @Component
 @Order(value = 2)
 public class TestOrder implements CommandLineRunner {
+    @Resource
+    private UserMapper userMapper;
     @Override
-
     public void run(String[] args) {
+
+        List<User> userList = userMapper.selectList(null);
+        userList.forEach(System.out::println);
         System.out.println("order2");
     }
 }
---
+++ ling/src/main/java/com/zfl/ling/dao/UserMapper.java
@@ -0,0 +1,15 @@
+package com.zfl.ling.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.zfl.ling.entity.User;
+
+/**
+ * @ClassName UserMapper
+ * @Description TODO
+ * @Author zzzzitai
+ * @Date 2019/5/15 15:23
+ * @Version 1.0
+ **/
+
+public interface UserMapper extends BaseMapper<User> {
+}
