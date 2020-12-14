e344386e40e26421311b0e1ab97d56e1445feeba
qinzhengzz <henuqin@gmail.com>
2020-12-14 10:17:16 +0800

    merge

--- ling/src/main/java/com/zfl/LingApplication.java
+++ ling/src/main/java/com/zfl/LingApplication.java
@@ -7,8 +7,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import java.util.ArrayList;
 import java.util.List;
 
-
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication
 public class LingApplication {
     public static void main(String[] args) {
         SpringApplication.run(LingApplication.class, args);
--- ling/src/main/java/com/zfl/controller/ProductExceptionController.java
+++ ling/src/main/java/com/zfl/controller/ProductExceptionController.java
@@ -1,9 +1,4 @@
-<<<<<<< HEAD:ling/src/main/java/com/zfl/controller/ProductExceptionController.java
-package com.zfl.ling.controller;
-=======
-package com.zfl.exception;
->>>>>>> a3e77b822afb1131892397cba4f3111267cf8f7c:ling/src/main/java/com/zfl/exception/ProductExceptionController.java
-
+package com.zfl.controller;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 
 /**
